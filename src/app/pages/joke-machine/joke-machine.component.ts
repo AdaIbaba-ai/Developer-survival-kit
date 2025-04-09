a5cdfb9a52a8862c8631ae527c7bccc33435179e
Adyla Ibadova <adyla.ibadova@bedag.ch>
2025-04-09 09:44:12 +0200

    Adding joke-machine component

---
+++ src/app/pages/joke-machine/joke-machine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-machine',
+  imports: [],
+  templateUrl: './joke-machine.component.html',
+  styleUrl: './joke-machine.component.css'
+})
+export class JokeMachineComponent {
+
+}
