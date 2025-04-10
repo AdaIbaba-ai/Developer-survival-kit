0e4b41a801f5e6a890bae88388c38d017dee4651
Adyla Ibadova <adyla.ibadova@bedag.ch>
2025-04-10 15:14:47 +0200

    Design upgrades

--- src/app/pages/alibi-generator/alibi-generator.component.ts
+++ src/app/pages/alibi-generator/alibi-generator.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './alibi-generator.component.html',
+  styleUrls: ['./alibi-generator.component.css'],
 })
 export class AlibiGeneratorComponent {
   excuses: string[] = [
